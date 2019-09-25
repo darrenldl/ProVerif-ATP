da95e05af67053948a853d9442363235d6d6d109
Darren Ldl <darrenldldev@gmail.com>
2019-09-26 00:21:35 +1000

    Adjusting parser code

--- narrator/src/vampire.ml
+++ narrator/src/vampire.ml
@@ -138,13 +138,17 @@ module Raw_line = struct
   open Parser_components
   open Printf
 
-  type info = {
-    descr : string;
-    parents : string list;
+  type extra_info = {
     l_node : string;
     r_node : string;
     l_ast_indices : int list;
     r_ast_indices : int list;
+  }
+
+  type info = {
+    descr : string;
+    parents : string list;
+    extra : extra_info option;
   }
 
   type line = string * Raw_expr.expr * info
@@ -176,21 +180,24 @@ module Raw_line = struct
     (fun descr ->
        sep_by (many1_chars digit) (char ',') >>=
        (fun parents ->
-          spaces >> non_digit_p >>=
+          (char ']' >> return { descr; parents; extra = None })
+          <|>
+          (spaces >> non_digit_p >>=
           (fun l_node ->
              string "into" >> non_digit_p >>=
              (fun r_node ->
                 char ',' >> spaces >> string "unify on" >> spaces >> string "(0)." >> sep_by (many1_chars digit) (char '.') >>=
                 (fun l_ast_indices ->
-                   spaces >> string "and" >> spaces >> string "(0)." >> sep_by (many1_chars digit) (char '.') >>=
+                   string "in" >> many1_chars digit >> spaces >> string "and" >> spaces >> string "(0)." >> sep_by (many1_chars digit) (char '.') >>=
                    (fun r_ast_indices ->
+                      string "in" >> many1_chars digit >> char ']' >>
                       let l_ast_indices = l_ast_indices |> List.map int_of_string in
                       let r_ast_indices = r_ast_indices |> List.map int_of_string in
-                      return { descr; parents; l_node; r_node; l_ast_indices; r_ast_indices}
+                      return { descr; parents; extra = Some {l_node; r_node; l_ast_indices; r_ast_indices} }
                    )
                 )
              )
-          )
+          ))
        )
     )
   
